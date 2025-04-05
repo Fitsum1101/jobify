53b0cebf7a4be038d5ff829953db5105e03b256b
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-05 22:03:07 +0300

    adding ,updating and deleteing admin logic implementation

--- routes/admin.js
+++ routes/admin.js
@@ -32,6 +32,47 @@ router.post("/admin", async (req, res, next) => {
   }
 });
 
+router.patch("/admin/edit/:id", async (req, res, next) => {
+  const id = +req.params.id;
+  const { email, username, password } = req.body;
+  const adminExist = await db.user.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!adminExist) {
+    return res.status(404).json({
+      msg: "Admin does't exists",
+    });
+  }
+  const adminEmailExists = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  if (adminEmailExists) {
+    return res.status(403).json({
+      msg: "email aiready in use!!",
+    });
+  }
+  const passwordHased = await bcrypt.hash(password, 12);
+  const updatedAdmin = await db.user.update({
+    where: {
+      id,
+    },
+    data: {
+      email,
+      username,
+      password: passwordHased,
+    },
+  });
+
+  return res.status(200).json({
+    updatedAdmin,
+  });
+});
+
 router.delete("/admin", async (req, res, next) => {
   try {
     const id = req.body.id;
