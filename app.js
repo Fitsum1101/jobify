5ff745b560fde69da8b413d73f9961cafc9a7e06
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-05 18:42:18 +0300

    adding updating files

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const express = require("express");
+import express from "express";
+
+import admiRoute from "./routes/admin.js";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 
-
+app.use(admiRoute);
 
 app.listen(3000);
