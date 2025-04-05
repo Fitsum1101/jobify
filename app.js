bf1f24015cd3ca23cd86e11b917ed80fe09620f8
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-05 21:06:10 +0300

    admin route logic and package imported

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const admiRoute = require("./routes/admin");
 
-const { PrismaClient } = require("@prisma/client");
-
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(admiRoute);
 
