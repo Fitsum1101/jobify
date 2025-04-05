373db9b081b03f8449b1927c022514d56a2312db
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-06 00:17:20 +0300

    multer implemetaion for company logo uploading

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const admiRoute = require("./routes/admin");
+const companyRoute = require("./routes/company");
 
 const app = express();
 
@@ -9,5 +10,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(admiRoute);
-
+app.use(companyRoute);
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+  });
+});
 app.listen(3000);
--- routes/company.js
+++ routes/company.js
@@ -1,11 +1,64 @@
 const express = require("express");
+const multer = require("multer");
 
 const db = require("../config/db");
 
 const router = express.Router();
 
-router.post()
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images/");
+  },
+  filename: (req, file, cb) => {
+    const fileName = Date.now() + "-" + file.originalname;
 
+    cb(null, fileName);
+  },
+});
 
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 1024 * 1024 * 5 },
+});
+
+router.post("/company", upload.single("file"), async (req, res, next) => {
+  try {
+    const name = req.body.name;
+    const file = req.file;
+
+    const nameExist = await db.companey.findUnique({
+      where: {
+        name,
+      },
+    });
+    if (nameExist) {
+      return res.status(403).json({
+        msg: "name aleady exists",
+      });
+    }
+    const newCompany = await db.companey.create({
+      data: {
+        logo: file.path,
+        name,
+      },
+    });
+    return res.status(403).json({ newCompany });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
