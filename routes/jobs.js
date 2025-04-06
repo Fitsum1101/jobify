2e86c4cc4f62d3f88ee92fd8f0e78fb8cf5ce159
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-06 15:10:52 +0300

    pdf uploading done successfuly

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,15 +1,43 @@
 const express = require("express");
+const multer = require("multer");
 
 const db = require("../config/db");
 
 const router = express.Router();
 
-router.post("/company/:name/jobs", async (req, res, next) => {
-  try {
-    const companeyName = req.params.name;
-  } catch (error) {
-    
-  }
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "data/");
+  },
+  filename: (req, file, cb) => {
+    const filename = Date.now() + "-" + file.originalname;
+    cb(null, filename);
+  },
 });
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype !== "application/pdf") {
+    return cb(null, false);
+  }
+  cb(null, true);
+};
+
+const uploadPdf = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 1024 * 1024 * 5 },
+});
+
+router.post(
+  "/company/:name/jobs",
+  uploadPdf.single("pdf"),
+  async (req, res) => {
+    try {
+      const companeyName = req.params.name;
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
