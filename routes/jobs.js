847c5a9afa7e169af2f55b40bae345739d9041fe
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-06 14:52:14 +0300

    appliying some modefying to the project

--- routes/jobs.js
+++ routes/jobs.js
@@ -4,8 +4,12 @@ const db = require("../config/db");
 
 const router = express.Router();
 
-
-
-
+router.post("/company/:name/jobs", async (req, res, next) => {
+  try {
+    const companeyName = req.params.name;
+  } catch (error) {
+    
+  }
+});
 
 module.exports = router;
