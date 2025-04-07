cf1582238027cd3d2e7836dcf731e42ec8ac7ba4
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-07 20:00:47 +0300

    adding commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+// amazing project
 app.use(admiRoute);
 app.use(companyRoute);
 app.use(jobsRoute);
