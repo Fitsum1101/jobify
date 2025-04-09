cfd88118b37f104ced4a0ebf26484d242ba2d9e6
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-09 09:53:43 +0300

    updating the file to integrating jobs logic

--- routes/jobs.js
+++ routes/jobs.js
@@ -2,7 +2,8 @@ const express = require("express");
 const multer = require("multer");
 
 const db = require("../config/db");
-// const 
+const ai = require("../config/ai");
+const { Jobtype } = require("@prisma/client");
 
 const router = express.Router();
 
@@ -32,51 +33,120 @@ const uploadPdf = multer({
 router.post(
   "/company/:name/jobs",
   uploadPdf.single("pdf"),
-  async (req, res) => {
+  async (req, res, next) => {
     try {
       const companeyName = req.params.name;
       const jobData = { ...req.body };
       const pdfFile = req.file;
-      const companyExist = await db.companey.findFirst({
+      const companyExist = await db.companey.findUnique({
         where: {
           name: companeyName,
         },
       });
       if (!companyExist) {
         return res.status(404).json({
-          msg: "not Founded",
+          msg: "Not Founded",
         });
       }
+      const questionData = `I don't want any talk just give me descrption for my job which have title of ${jobData.title} ,requirement ${jobData.requirments} ,salary ${jobData.salary} ,location ${jobData.location} and  job working time ${jobData.jobtype} make it mideum size(less than < 191) !!!! `;
+      const response = await ai.models.generateContent({
+        model: "gemini-2.0-flash-001",
+        contents: questionData,
+      });
+      jobData["description"] = response.candidates[0].content.parts[0].text;
       if (pdfFile) {
         jobData["descFile"] = req.file.path;
       }
-
-      const questionData = `Write me job description for this java script object data that it have all data you need for generating the description info ${jobData} `;
-
-
-
+      const jobInfo = await db.job.create({
+        data: {
+          ...{ ...jobData, companey_id: companyExist.id },
+        },
+      });
+      res.status(201).json({
+        jobInfo,
+      });
     } catch (error) {
       next(error);
     }
   }
 );
 
-// router.patch(
-//   "/company/:name/jobs/edit/:id",
-//   uploadPdf.single("pdf"),
-//   async (req, res, next) => {
-//     const companeyName = req.params.name;
-//     const datas = { ...req.body };
-//     const file = req.file;
-//     if (file) {
-//       //  update the file that exists
-//     }
-//     const jobExists = await db.job.findUnique({
-//       where: {
-//         title: companeyName,
-//       },
-//     });
-//   }
-// );
+router.patch(
+  "/company/:name/jobs/edit/:id",
+  uploadPdf.single("pdf"),
+  async (req, res, next) => {
+    const companeyName = req.params.name;
+    const id = +req.params.id;
+    const jobData = { ...req.body };
+    const pdfFile = req.file;
+    const companyExist = await db.companey.findUnique({
+      where: {
+        name: companeyName,
+      },
+    });
+    const jobExis = await db.job.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!companyExist || !jobExis) {
+      return res.status(404).json({
+        msg: "Not Founded",
+      });
+    }
+    if (pdfFile) {
+      jobData["descFile"] = req.file.path;
+    } else {
+      jobData["descFile"] = jobExis.descFile;
+    }
+    const questionData = `I don't want any talk just give me descrption for my job which have title of ${jobData.title} ,requirement ${jobData.requirments} ,salary ${jobData.salary} ,location ${jobData.location} and  job working time ${jobData.jobtype} make it mideum size(less than < 191) !!!! `;
+    const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash-001",
+      contents: questionData,
+    });
+    jobData["description"] = response.candidates[0].content.parts[0].text;
+    const updatedJob = await db.job.update({
+      where: {
+        id,
+      },
+      data: {
+        ...jobData,
+      },
+    });
+    res.status(200).json({
+      updatedJob,
+    });
+  }
+);
+
+router.delete("/company/:name/jobs/delete/:id", async (req, res, next) => {
+  const id = +req.params.id;
+  const deletedJob = await db.job.delete({
+    where: {
+      id,
+    },
+  });
+  res.status(200).json(deletedJob);
+});
+
+// filter by using jobtitle,salary and location
+router.get("/company/jobs/", async (req, res, next) => {
+  const { title, salary, location } = req.query;
+  console.log(title, salary, location);
+  let filterMethod = {};
+  if (title) {
+    filterMethod = { title };
+  }
+  if (salary) {
+    filterMethod = { salary: +salary };
+  }
+  if (location) {
+    filterMethod = { location };
+  }
+  const jobs = await db.job.findMany({
+    where: { ...filterMethod },
+  });
+  res.status(200).json(jobs);
+});
 
 module.exports = router;
