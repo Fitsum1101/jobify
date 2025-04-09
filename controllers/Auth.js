c72cd8bd22ab11fca0d3fdf87202855884862cf9
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-09 15:18:33 +0300

    adding authtication

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -3,8 +3,20 @@ const JWT = require("jsonwebtoken");
 
 exports.postLogin = async (req, res, next) => {
   const { email, password } = req.body;
-  res.status(200).json({
-    email,
-    password,
+
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      username: true,
+      email: true,
+      id: true,
+    },
+  });
+  const token = JWT.sign(user, "mysecretkey", { expiresIn: "1h" });
+  return res.status(200).json({
+    msg: "login successifly🌼",
+    token,
   });
 };
