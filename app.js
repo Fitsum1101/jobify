530918863f3d54a3f82f779a5123dc397eb4e8c6
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-09 13:41:06 +0300

    adding authcontrolers auth

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const admiRoute = require("./routes/admin");
 const companyRoute = require("./routes/company");
 const jobsRoute = require("./routes/jobs");
+const AuthRoute = require("./routes/Auth");
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(bodyParser.json());
 app.use(admiRoute);
 app.use(companyRoute);
 app.use(jobsRoute);
+app.use(AuthRoute);
 
 app.use((err, req, res, next) => {
   res.status(500).json({
