1eda5ed2ba3fc0d918bc7fcd41d416feae394898
Fitsum Kifle <fitsumt835@gmail.com>
2025-04-09 10:05:32 +0300

    admin controllers file updated

--- routes/admin.js
+++ routes/admin.js
@@ -1,102 +1,14 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 
-const db = require("../config/db");
+const adminCotroller = require("../controllers/admin");
 
 const router = express.Router();
 
-router.post("/admin", async (req, res, next) => {
-  try {
-    const { username, email, password } = req.body;
-    const adminExist = await db.user.findFirst({
-      where: {
-        email,
-      },
-    });
-    if (adminExist) {
-      return res.status(403).json({
-        msg: "email aiready in use!!",
-      });
-    }
-    const bcryptPassword = await bcrypt.hash(password, 12);
-    const newAdmin = await db.user.create({
-      data: {
-        username,
-        email,
-        password: bcryptPassword,
-      },
-    });
-    return res.status(201).json({ newAdmin });
-  } catch (error) {
-    next(error);
-  }
-});
+router.post("/admin", adminCotroller.postAdmin);
 
-router.patch("/admin/edit/:id", async (req, res, next) => {
-  const id = +req.params.id;
-  const { email, username, password } = req.body;
-  const adminExist = await db.user.findUnique({
-    where: {
-      id,
-    },
-  });
+router.patch("/admin/edit/:id", adminCotroller.patchEditAdmin);
 
-  if (!adminExist) {
-    return res.status(404).json({
-      msg: "Admin does't exists",
-    });
-  }
-  const adminEmailExists = await db.user.findUnique({
-    where: {
-      email,
-    },
-  });
-  if (adminEmailExists) {
-    return res.status(403).json({
-      msg: "email aiready in use!!",
-    });
-  }
-  const passwordHased = await bcrypt.hash(password, 12);
-  const updatedAdmin = await db.user.update({
-    where: {
-      id,
-    },
-    data: {
-      email,
-      username,
-      password: passwordHased,
-    },
-  });
-
-  return res.status(200).json({
-    updatedAdmin,
-  });
-});
-
-router.delete("/admin", async (req, res, next) => {
-  try {
-    const id = req.body.id;
-    const adminExists = await db.user.findUnique({
-      where: {
-        id,
-      },
-    });
-    if (!adminExists) {
-      return res.status(404).json({
-        msg: "does't exists!",
-      });
-    }
-    const deletedAdmin = await db.user.delete({
-      where: {
-        id,
-      },
-    });
-    res.status(200).json({
-      deletedAdmin,
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete("/admin", adminCotroller.deleteAdimn);
 
 module.exports = router;
